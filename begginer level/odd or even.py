6a79fe10af7e567c74c680ae2df8bf15ca9aa651
irahkrishnan <38775752+irahkrishnan@users.noreply.github.com>
2018-04-27 10:24:43 +0530

    Update odd or even.py

--- begginer level/odd or even.py
+++ begginer level/odd or even.py
@@ -1,6 +1,8 @@
 num = input("enter the input\n)
 if(isinstance(num.(int.float)));
-if(num %2==0);
+if(num % 2 ==0);
 print("the given number is even")
 else:
 print("the given is odd")
+            else:
+            print("invalid input")
